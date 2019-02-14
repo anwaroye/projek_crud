560ef4d98744fc27d205663c2f13512360a32bee
anwaroye <anwaroye68@gmail.com>
2019-02-15 02:51:32 +0700

    new seeder

--- database/seeds/DropdownSeeder.php
+++ database/seeds/DropdownSeeder.php
@@ -11,6 +11,17 @@ class DropdownSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('type_benda')->insert([
+          'kat_benda' => 'elektronik',
+        ]);
+
+        DB::table('jeniskelamins')->insert([
+          'jenis_kelamin'=> 'laki-laki'
+        ]);
+        
+        DB::table('position_managers')->insert([
+         'position_manag' => 'pemilik'
+        ]);
+
     }
 }
