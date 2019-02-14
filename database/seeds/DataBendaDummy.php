628c00c9ee745b90ad29a0d6397d4b16cccce5fc
anwaroye <anwaroye68@gmail.com>
2019-02-14 09:18:25 +0700

    make seeder

--- database/seeds/DataBendaDummy.php
+++ database/seeds/DataBendaDummy.php
@@ -11,6 +11,12 @@ class DataBendaDummy extends Seeder
      */
     public function run()
     {
-        //
-    }
+      DB::table('table_objects')->insert([
+        'id_object'=> '7ff261ba-d209-480f-80da-1d1bccc440bb',
+        'object_name' => 'telepon',
+        'type_object'=> 'elektronik',
+        'object_desc'=>'anjin',
+        'object_img' => 'frame (1).png'
+      ]);
+  }
 }
